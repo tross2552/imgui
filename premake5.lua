bda5a0d36527ad13e1df4c12e59a3e097a49a3f3
Trent Ross <tross2552@gmail.com>
2021-06-15 17:07:54 -0400

    Add premake build script.

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "on"
+  systemversion "latest"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+    "imgui.h",
+    "imconfig.h",
+    "imgui_internal.h",
+    "imstb_rectpack.h",
+    "imstb_textedit.h",
+    "imstb_truetype.h",
+		"imgui.cpp",
+		"imgui_demo.cpp",
+		"imgui_draw.cpp",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp"
+	}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
